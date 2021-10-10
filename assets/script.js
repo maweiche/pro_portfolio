eafed68afea147f45ec782b7d0c4096969826f89
Matt Weichel <maweiche@gmail.com>
2021-10-10 13:28:50 -0700

    html/js, added structure for modal to present contact form

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,8 @@
 var name = document.getElementById("mainName");
+var modal = document.getElementById("contactModal");
+
+function displayModal () {
+    modal.style.display="inline";
+}
+
+displayModal();
