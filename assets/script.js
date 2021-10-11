e7e53bfda5aa8f816001202ecc891743ae6e01bb
Matt Weichel <maweiche@gmail.com>
2021-10-10 17:36:58 -0700

    html/js, resolved bootstrap js link issue, modal now popping up, still needs styling/formatting

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,20 @@
 var name = document.getElementById("mainName");
-var modal = document.getElementById("contactModal");
+// var contactModal = document.getElementById("contactModal");
 
-function displayModal () {
-    modal.style.display="inline";
-}
+// $('#contactModal').on('show.bs.modal', function(event) {
+//     var button = $(event.relatedTarget)  //Btn that triggered modal
+//     var recipient = button.data('whatever') //Pull info from data attributes
+//     var modal = $(this)
+//     modal.find('.modal-title').text('New message to ' + recipient)
+//     modal.find('.modal-body input').val(recipient)
+// });
 
-displayModal();
+$('#contactModal').on('show.bs.modal', function (event) {
+    var button = $(event.relatedTarget) // Button that triggered the modal
+    var recipient = button.data('whatever') // Extract info from data-* attributes
+    // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+    // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+    var modal = $(this)
+    modal.find('.modal-title').text('New message to ' + recipient)
+    modal.find('.modal-body input').val(recipient)
+  })
